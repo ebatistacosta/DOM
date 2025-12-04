3f2a2b9e9a3557e8f2bb6e60fe8e0b8486631880
ebatistacosta <erikabatistacostah@gmail.com>
2025-12-04 17:05:43 -0300

    Ex.6 inicio

---
+++ DOM/6/script.js
@@ -0,0 +1,6 @@
+const titulo = document.getElementById("titulo");
+const modo = document.getElementById("modo");
+
+modo.addEventListener("click", function(){
+    titulo.textContent = "Dark Mode";
+});
